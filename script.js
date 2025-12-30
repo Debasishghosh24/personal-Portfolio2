edf3b9f11926297b5c2bb5196d916747f2253d30
Debasish ghosh <119937911+Debasishghosh24@users.noreply.github.com>
2025-12-30 19:03:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+var typeSwitch=new Typed(".role",{
+    strings:["frontend Developer","web Designer", "software Engineer", "coder"],
+    typeSpeed:50,
+    backSpeed:50,
+    backDelay:1000,
+    loop:true,
+    showCursor: false,
+});
